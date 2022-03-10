e89ab03d377bd6ee7bcdf2757249756c0fd72cc9
Germain Bergeron <gbergeron@coveo.com>
2022-03-10 14:10:39 -0500

    test: setup test runner

---
+++ jest.config.mjs
@@ -0,0 +1,27 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+  extensionsToTreatAsEsm: [".ts"],
+  preset: "ts-jest/presets/default-esm",
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/**/*.{ts,tsx}"],
+  coverageDirectory: "coverage",
+
+  rootDir: "./src",
+
+  testEnvironment: "node",
+};
