4daa68370b0db2b63516ecac8a23ac4c7f151e84
Louis Bompart <lbompart@coveo.com>
2022-03-29 22:45:48 -0400

    use util
    Co-authored-by: Germain Bergeron <gbergeron@coveo.com>

--- src/git/doCommitTree.ts
+++ src/git/doCommitTree.ts
@@ -1,15 +1,14 @@
+import getOptionalArgument from "../utils/getOptionalArgument.js";
 import spawnSync from "../utils/spawnSync.js";
 import gitLogger from "./utils/gitLogger.js";
 
 export default function (tree: string, parent?: string, message?: string) {
   return spawnSync(
     "git",
-    [
-      "commit-tree",
-      tree,
-      ...(parent ? ["-p", parent] : []),
-      ...(message ? ["-m", message] : []),
-    ],
+    ["commit-tree", tree].concat(
+      getOptionalArgument("-p", parent),
+      getOptionalArgument("-m", message)
+    ),
     gitLogger
   ).stdout.trim();
 }
