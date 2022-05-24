183b10aee4a29d2fb8ff8c0deb96d17cac86fc36
Louis Bompart <lbompart@coveo.com>
2022-05-24 17:17:48 +0200

    cic(cd): refs take a s

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -177,7 +177,7 @@ import retry from "async-retry";
   await octokit.rest.git.updateRef({
     owner: REPO_OWNER,
     repo: REPO_NAME,
-    ref: "ref/heads/main",
+    ref: "refs/heads/main",
     sha: commit.data.sha,
   });
   // Finally, delete the temp branch.
