d5b4c6d8c2bfe25f13fc716f548a677ba935b419
Louis Bompart <lbompart@coveo.com>
2022-06-06 13:49:20 -0400

    feat(pnpm): remove support for pnpm6 (#45)

--- src/pnpm/utils/runOnChanged.ts
+++ src/pnpm/utils/runOnChanged.ts
@@ -1,4 +1,3 @@
-import semver from "semver";
 import spawn from "../../utils/spawn.js";
 import getExclusionFilters from "./getExclusionFilters.js";
 import getIncludeFilters from "./getIncludeFilters.js";
@@ -10,17 +9,12 @@ export const runOnChanged = async (
   forcePackages: string[],
   excludePackages: string[]
 ) => {
-  const pnpmVersion = semver.parse(
-    (await spawn("pnpm", ["--version"], pnpmLogger)).stdout.trim()
-  );
   const pnpmArgs = [
     "--recursive",
     `--filter="...[${since}]"`,
     ...getIncludeFilters(forcePackages),
     ...getExclusionFilters(excludePackages),
     "exec",
-    // pnpm@6 requires "--" before the command, pnpm@7 doesn't
-    ...(pnpmVersion.major === 6 ? ["--"] : []),
     cmd,
   ];
   return spawn("pnpm", pnpmArgs, pnpmLogger, {
