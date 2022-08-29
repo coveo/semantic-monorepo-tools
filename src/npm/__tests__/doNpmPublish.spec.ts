29107b15a7ed411d3b164bcd2d34d550d00b204f
Louis Bompart <lbompart@coveo.com>
2022-08-29 18:08:50 -0400

    feat(npm): add tag flag support for npm publish

---
+++ src/npm/__tests__/doNpmPublish.spec.ts
@@ -0,0 +1,58 @@
+import type { Readable } from "node:stream";
+import { spawn, ChildProcess } from "node:child_process";
+import { EventEmitter } from "node:events";
+import npmPublish from "../doNpmPublish.js";
+import appendCmdIfWindows from "../utils/appendCmdIfWindows.js";
+
+jest.mock("node:child_process");
+const mockedSpawn = jest.mocked(spawn, true);
+
+jest.mock("../utils/appendCmdIfWindows.js");
+jest
+  .mocked(appendCmdIfWindows, true)
+  .mockImplementation((string: string) => string);
+
+const doMockDummySpawn = () => {
+  mockedSpawn.mockImplementation(() => {
+    const cpEventEmitter: ChildProcess = new EventEmitter() as ChildProcess;
+    const stdoutEventEmitter = new EventEmitter();
+    const stderrEventEmitter = new EventEmitter();
+    cpEventEmitter.stdout = stdoutEventEmitter as Readable;
+    cpEventEmitter.stderr = stderrEventEmitter as Readable;
+    setTimeout(() => {
+      cpEventEmitter.emit("close", 0);
+    }, 0);
+    return cpEventEmitter;
+  });
+};
+
+describe("npmPublish()", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    doMockDummySpawn();
+  });
+
+  describe("when no npmOpts is given", () => {
+    it("calls `npm publish`", async () => {
+      await npmPublish("somepath");
+
+      expect(mockedSpawn).toHaveBeenCalledWith(
+        appendCmdIfWindows`npm`,
+        ["publish"],
+        { cwd: "somepath" }
+      );
+    });
+  });
+
+  describe("when npmOpts.tag is given", () => {
+    it("calls `npm publish` with the --tag flag", async () => {
+      await npmPublish("somepath", { tag: "sometag" });
+
+      expect(mockedSpawn).toHaveBeenCalledWith(
+        appendCmdIfWindows`npm`,
+        ["publish", "--tag", "sometag"],
+        { cwd: "somepath" }
+      );
+    });
+  });
+});
--- src/npm/doNpmPublish.ts
+++ src/npm/doNpmPublish.ts
@@ -1,9 +1,25 @@
+import getOptionalFlagArgument from "../utils/getOptionalFlagArgument.js";
 import spawn from "../utils/spawn.js";
 import appendCmdIfWindows from "./utils/appendCmdIfWindows.js";
 import npmLogger from "./utils/npmLogger.js";
 
-export default async function (PATH: string) {
-  await spawn(appendCmdIfWindows`npm`, ["publish"], npmLogger, {
+interface NpmOptions {
+  /**
+   * If you ask npm to install a package and don't tell it a specific version, then it will install the specified tag.
+   * Also the tag that is added to the `package@version` specified by the npm tag command, if no explicit tag is given.
+   * When used by the npm diff command, this is the tag used to fetch the tarball that will be compared with the local files by default.
+   * @default "latest"
+   * @see {@link https://docs.npmjs.com/cli/v8/commands/npm-publish#tag}
+   */
+  tag: string;
+}
+
+export default async function (PATH: string, npmOpts?: NpmOptions) {
+  const params = ["publish"];
+  for (const opt in npmOpts) {
+    params.push(...getOptionalFlagArgument(`--${opt}`, npmOpts[opt]));
+  }
+  await spawn(appendCmdIfWindows`npm`, params, npmLogger, {
     cwd: PATH,
   });
 }
