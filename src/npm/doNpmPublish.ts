fe404ddb6d4166d0615fee1d6edeeac35e80835f
Louis Bompart <lbompart@coveo.com>
2022-08-30 13:02:12 -0400

    feat(npm): add tag flag support for npm publish (#79)
    * feat(npm): add tag flag support for npm publish
    
    * Update src/npm/doNpmPublish.ts
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>
    
    * Update src/npm/doNpmPublish.ts
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>
    
    * Update src/npm/doNpmPublish.ts
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>

--- src/npm/doNpmPublish.ts
+++ src/npm/doNpmPublish.ts
@@ -1,9 +1,23 @@
+import getOptionalFlagArgument from "../utils/getOptionalFlagArgument.js";
 import spawn from "../utils/spawn.js";
 import appendCmdIfWindows from "./utils/appendCmdIfWindows.js";
 import npmLogger from "./utils/npmLogger.js";
 
-export default async function (PATH: string) {
-  await spawn(appendCmdIfWindows`npm`, ["publish"], npmLogger, {
+interface NpmOptions {
+  /**
+   * Specify the tag to publish on
+   * @default "latest"
+   * @see {@link https://docs.npmjs.com/cli/v8/commands/npm-publish#tag}
+   */
+  tag: string;
+}
+
+export default async function (PATH: string, npmOpts?: NpmOptions) {
+  const params = ["publish"];
+  for (const opt in npmOpts) {
+    params.push(...getOptionalFlagArgument(`--${opt}`, npmOpts[opt]));
+  }
+  await spawn(appendCmdIfWindows`npm`, params, npmLogger, {
     cwd: PATH,
   });
 }
