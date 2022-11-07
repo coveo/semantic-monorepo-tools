17b4ba60f7bdf5348d0ce95fa9a655eabcb1a0c2
Louis Bompart <lbompart@coveo.com>
2022-11-07 10:07:08 -0500

    feat(npm): add describeNpmTag function (#106)
    Add a function that leverage npm show to get the latest version of a tag of a package.

---
+++ src/npm/describeNpmTag.ts
@@ -0,0 +1,9 @@
+import spawn from "../utils/spawn.js";
+import appendCmdIfWindows from "./utils/appendCmdIfWindows.js";
+import npmLogger from "./utils/npmLogger.js";
+
+export default async function (packageName: string, tag = "latest") {
+  const params = ["view", [packageName, tag].join("@"), "version"];
+  const npmPs = await spawn(appendCmdIfWindows`npm`, params, npmLogger);
+  return npmPs.stdout.toString();
+}
