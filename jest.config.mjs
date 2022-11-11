5d996692e27925a1e6615963529d77d64d60fda7
Louis Bompart <lbompart@coveo.com>
2022-11-11 07:12:15 -0500

    fix(npm): describeNpmTag, trim output (#111)
    * Update describeNpmTag.ts
    
    * ut

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,7 +6,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
   extensionsToTreatAsEsm: [".ts"],
-  preset: "ts-jest/presets/default-esm-legacy",
+  preset: "ts-jest/presets/default-esm",
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
--- src/npm/describeNpmTag.ts
+++ src/npm/describeNpmTag.ts
@@ -5,5 +5,5 @@ import npmLogger from "./utils/npmLogger.js";
 export default async function (packageName: string, tag = "latest") {
   const params = ["view", [packageName, tag].join("@"), "version"];
   const npmPs = await spawn(appendCmdIfWindows`npm`, params, npmLogger);
-  return npmPs.stdout.toString();
+  return npmPs.stdout.toString().trim();
 }
