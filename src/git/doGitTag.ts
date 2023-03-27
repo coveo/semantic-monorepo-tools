84f32253775d939a62b310904d12fa5989d0c3e6
Louis Bompart <lbompart@coveo.com>
2023-03-27 13:06:37 -0400

    feat(git): add support for ref tagging

--- src/git/doGitTag.ts
+++ src/git/doGitTag.ts
@@ -1,6 +1,11 @@
+import getOptionalPositionalArgument from "../utils/getOptionalPositionalArgument.js";
 import spawn from "../utils/spawn.js";
 import gitLogger from "./utils/gitLogger.js";
 
-export default async function (tag: string) {
-  await spawn(`git`, ["tag", tag], gitLogger);
+export default async function (tag: string, commitSHA?: string) {
+  await spawn(
+    `git`,
+    ["tag", tag].concat(getOptionalPositionalArgument(commitSHA)),
+    gitLogger
+  );
 }
