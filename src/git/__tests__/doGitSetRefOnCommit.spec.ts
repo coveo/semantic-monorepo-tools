662f4ed2a9c42d2a8bd0a8ef61ebb4e6e52ec9be
Louis Bompart <lbompart@coveo.com>
2023-04-04 09:58:20 -0400

    feat(git): allow force flag on gitSetRef (#148)

--- src/git/__tests__/doGitSetRefOnCommit.spec.ts
+++ src/git/__tests__/doGitSetRefOnCommit.spec.ts
@@ -27,4 +27,19 @@ describe("doGitSetRef()", () => {
       });
     }
   );
+
+  it("should append --force when force=true", async () => {
+    await gitSetRef("origin", "a", "b", true);
+    expect(mockedSpawn.mock.lastCall[1].pop()).toBe("--force");
+  });
+
+  it("should not append --force when force is unset", async () => {
+    await gitSetRef("origin", "a", "b");
+    expect(mockedSpawn.mock.lastCall[1].pop()).not.toBe("--force");
+  });
+
+  it("should not append --force when force=false", async () => {
+    await gitSetRef("origin", "a", "b", false);
+    expect(mockedSpawn.mock.lastCall[1].pop()).not.toBe("--force");
+  });
 });
