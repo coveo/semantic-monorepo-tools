612a50e5988355a4b671532a11b2f8f0c9615914
Louis Bompart <lbompart@coveo.com>
2023-04-11 16:42:45 -0400

    jsdoc

--- src/git/doGitPush.ts
+++ src/git/doGitPush.ts
@@ -1,9 +1,27 @@
 import spawn from "../utils/spawn.js";
 import gitLogger from "./utils/gitLogger.js";
 
+/**
+ * Parameters of `gitPush`
+ */
 interface GitPushParams {
+  /**
+   * Use `--force` flag if true
+   * @type {boolean}
+   * @default [false]
+   */
   force?: boolean;
+  /**
+   * Which remote to push to
+   * @see https://git-scm.com/docs/git-push#_remotes
+   * @default {'origin'}
+   */
   remote?: string;
+  /**
+   * Which refs to push
+   * @see {@link https://git-scm.com/docs/git-push#Documentation/git-push.txt-ltrefspecgt82308203| Git-SCM, git push, refspec}
+   * @default {[]} @see {@link https://git-scm.com/docs/git-config#Documentation/git-config.txt-pushdefault| git config, push.default}}
+   */
   refs?: string[];
 }
 
