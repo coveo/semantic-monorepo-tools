c6e2f066f10a07989a7faaf089db8ada2fd15b5f
Louis Bompart <lbompart@coveo.com>
2023-04-13 08:49:36 -0400

    ci: use GH App authentication (#152)

--- scripts/get-scan-results.mjs
+++ scripts/get-scan-results.mjs
@@ -2,7 +2,18 @@ import { Octokit } from "octokit";
 
 const owner = "coveo";
 const repo = "semantic-monorepo-tools";
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+const authSecrets = {
+  appId: process.env.RELEASER_APP_ID,
+  privateKey: process.env.RELEASER_PRIVATE_KEY,
+  clientId: process.env.RELEASER_CLIENT_ID,
+  clientSecret: process.env.RELEASER_CLIENT_SECRET,
+  installationId: process.env.RELEASER_INSTALLATION_ID,
+};
+
+const octokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: authSecrets,
+});
 const alerts = await octokit.rest.dependabot.listAlertsForRepo({
   owner,
   repo,
