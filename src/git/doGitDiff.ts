830e1aa2a898d9efbcc0488cc69e96e2d498c1ee
Sam Thomas <sthomas@coveo.com>
2023-11-29 11:26:38 +0000

    feat: no pager

--- src/git/doGitDiff.ts
+++ src/git/doGitDiff.ts
@@ -2,5 +2,5 @@ import spawn from "../utils/spawn.js";
 import gitLogger from "./utils/gitLogger.js";
 
 export default async function gitDiff() {
-  await spawn("git", ["diff"], gitLogger);
+  await spawn("git", ["--no-pager", "diff", "--name-only"], gitLogger);
 }
