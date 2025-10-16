608f44d230a0fcabbb0f2599b42657e986b3ebc9
Olivier Lamothe <olamothe@coveo.com>
2025-10-16 13:39:44 -0400

    refactor tests

--- src/pnpm/__tests__/doRefreshPnpmLockfile.spec.ts
+++ src/pnpm/__tests__/doRefreshPnpmLockfile.spec.ts
@@ -1,29 +1,14 @@
-import type { Readable } from "node:stream";
-import { spawn, ChildProcess } from "node:child_process";
-import { EventEmitter } from "node:events";
+import { spawn } from "node:child_process";
 import refreshPnpmLockfile from "../doRefreshPnpmLockfile.js";
+import { mockSpawnSuccess } from "../../test-helpers/mockSpawn.js";
 
 jest.mock("node:child_process");
 const mockedSpawn = jest.mocked(spawn);
 
-const doMockDummySpawn = () => {
-  mockedSpawn.mockImplementation(() => {
-    const cpEventEmitter: ChildProcess = new EventEmitter() as ChildProcess;
-    const stdoutEventEmitter = new EventEmitter();
-    const stderrEventEmitter = new EventEmitter();
-    cpEventEmitter.stdout = stdoutEventEmitter as Readable;
-    cpEventEmitter.stderr = stderrEventEmitter as Readable;
-    setTimeout(() => {
-      cpEventEmitter.emit("close", 0);
-    }, 0);
-    return cpEventEmitter;
-  });
-};
-
 describe("refreshPnpmLockfile()", () => {
   beforeEach(() => {
     jest.resetAllMocks();
-    doMockDummySpawn();
+    mockSpawnSuccess(mockedSpawn);
   });
 
   it("calls `pnpm install --lockfile-only`", async () => {
